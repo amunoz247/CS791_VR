484a04495841d99d7fe764146fb298f058ed3b06
Andrew Munoz <amunoz247@users.noreply.github.com>
2019-02-06 01:01:40 -0800

    Created Kill Target script
    Added script that causes an object to explode when gaze meets certain time limit

--- P1/Assets/Scripts/LookMoveTo.cs
+++ P1/Assets/Scripts/LookMoveTo.cs
@@ -7,12 +7,14 @@ public class LookMoveTo : MonoBehaviour {
 	void Update () {
         Transform camera = Camera.main.transform;
         Ray ray;
-        RaycastHit hit;
+        RaycastHit[] hits;
         GameObject hitObject;
 
         Debug.DrawRay(camera.position, camera.rotation * Vector3.forward * 100.0f);
         ray = new Ray(camera.position, camera.rotation * Vector3.forward);
-        if (Physics.Raycast (ray, out hit)) {
+        hits = Physics.RaycastAll(ray);
+        for(int i = 0; i < hits.Length; i++) {
+            RaycastHit hit = hits[i];
             hitObject = hit.collider.gameObject;
             if (hitObject == ground) {
                 Debug.Log("Hit (x,y,z): " + hit.point.ToString("F2"));
