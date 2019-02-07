08f75a5befb85a13e110ff6594372c258fabf3b9
Andrew Munoz <amunoz247@users.noreply.github.com>
2019-02-07 13:25:56 -0800

    Created Input script
    Created new input script for controllers

---
+++ P1/Assets/Scripts/MyInputController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MyInputController : MonoBehaviour
+{
+    public SteamVR_TrackedObject rightHand;
+    private SteamVR_Controller.Device device;
+    
+    void Update()
+    {
+        ButtonTest();
+    }
+
+    private void ButtonTest()
+    {
+        string msg = null;
+
+        // SteamVR
+        device = SteamVR_Controller.Input((int)rightHand.index);
+        if (device != null && device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            msg = "Trigger press";
+            device.triggerHapticPulse(700);
+        }
+
+        if (device != null && device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            msg = "Trigger release";
+        }
+
+        if (msg != null)
+            Debug.Log("Input: " + msg);
+    }
+
+    public bool ButtonDown()
+    {
+        return Input.GetButtonDown("Fire1");
+    }
+
+    public bool ButtonUp()
+    {
+        return Input.GetButtonUp("Fire1");
+    }
+}
